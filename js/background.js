35f62bca3ac032e81d2a6eeb6d7837c2c0b0c465
Grewer <grewer@grewer.cn>
2018-07-05 18:14:10 +0800

    feat(module):右键加入黑名单的思路

--- js/background.js
+++ js/background.js
@@ -1,11 +1,24 @@
 console.log('run')
 
 
-chrome.contextMenus.create({
-  "title": "选中文字",
-  "contexts": ["selection"],
-  "onclick": function (e) {
-    alert('您点击了右键菜单！');
-    console.log('click', e)
-  }
-});
+function select(e) {
+  // alert('您点击了右键菜单！');
+  console.log('click', e)
+  // todo confirm 是否加入黑名单
+  // 页面地址: e.pageUrl
+  // 图片地址: e.srcUrl
+  // 是否为图片 e.mediaType === "image"
+
+  // 将其存储为本地数据 // 当前页无法触发dom操作 待解决
+
+  // 加载时搜索图片 当路径相同时,查看当前dom 的id 加入黑名单,若无class,id,则向其父元素搜索 ,直到父元素为3层以上或为body元素时停止
+  // 将其加入黑名单
+}
+
+// chrome.contextMenus.create({
+//   "title": "选中文字",
+//   "contexts": ["selection"],
+//   "onclick": select
+// });
+
+chrome.contextMenus.create({"title": "链接地址", "contexts": ["link"], "onclick": select});
