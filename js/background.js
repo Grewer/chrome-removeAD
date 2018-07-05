62907ee6ebacd5d1d935bebedd80ceb3e879ee5c
zhangyu <grewer@grewer.cn>
2018-07-05 23:31:18 +0800

    feat(module) : 添加消息传递,和规则存储

--- js/background.js
+++ js/background.js
@@ -2,17 +2,49 @@ console.log('run')
 
 
 function select(e) {
-  // alert('您点击了右键菜单！');
-  console.log('click', e)
-  // todo confirm 是否加入黑名单
   // 页面地址: e.pageUrl
   // 图片地址: e.srcUrl
   // 是否为图片 e.mediaType === "image"
 
-  // 将其存储为本地数据 // 当前页无法触发dom操作 待解决
+  // 将其存储为本地数据 // 当前页通过消息触发dom操作
 
   // 加载时搜索图片 当路径相同时,查看当前dom 的id 加入黑名单,若无class,id,则向其父元素搜索 ,直到父元素为3层以上或为body元素时停止
   // 将其加入黑名单
+  console.log(e)
+
+  if(e.mediaType !== "image") return;
+
+  var r = confirm("确认将此链接加入黑名单")
+  if (r) {
+    // 向 content-script.js 发送消息
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        method: 'rmImage',
+        message: e.srcUrl
+      }, function (response) {});
+    });
+
+
+
+    chrome.storage.sync.get({black: {}}, function (items) {
+      console.log(items)
+      var result = items.black[e.pageUrl]
+      if (result) {
+        if (result.indexOf(e.srcUrl) === -1) {
+          result.push(e.srcUrl)
+        }
+
+      } else {
+        items.black[e.pageUrl] = [e.srcUrl]
+      }
+      console.log(items)
+      chrome.storage.sync.set(items, function () {
+        console.log('保存成功！');
+      });
+
+    });
+  }
+
 }
 
 // chrome.contextMenus.create({
@@ -21,4 +53,4 @@ function select(e) {
 //   "onclick": select
 // });
 
-chrome.contextMenus.create({"title": "链接地址", "contexts": ["link"], "onclick": select});
+chrome.contextMenus.create({"title": "去除该广告", "contexts": ["link"], "onclick": select});
--- js/content-script.js
+++ js/content-script.js
@@ -1,1 +1,12 @@
 console.log(document) // it run
+chrome.storage.sync.get({black: ''}, function(items) {
+  console.log(items)
+});
+
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message.method === 'rmImage') {
+    console.log(message.message);
+    // 遍历 图片 链接 将路径屏蔽
+  }
+});
